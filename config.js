2903786002da73cd3e6c0c7e78efd3bf442b7026
fontbonalee <545593122@qq.com>
2016-12-21 14:10:01 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "FONTBONA",
+  short_description : "User interface and icon designer.",
+  email_address     : "545593122@qq.com.com"
+
+}
